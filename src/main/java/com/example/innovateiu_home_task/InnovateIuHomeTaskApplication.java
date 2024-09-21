b653cb31618449286637c8dde0b8ed92f7e4470d
Dmitriy Kravchuk <dmitriy.n.kravchuk@gmail.com>
2024-09-21 19:04:25 +0300

    Project_is_empty

---
+++ src/main/java/com/example/innovateiu_home_task/InnovateIuHomeTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.innovateiu_home_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovateIuHomeTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnovateIuHomeTaskApplication.class, args);
+    }
+
+}
