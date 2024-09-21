3ce4a5b62e2afa040bec3776a392ce1425387a09
Dmitriy Kravchuk <dmitriy.n.kravchuk@gmail.com>
2024-09-21 19:04:25 +0300

    Project_is_empty

---
+++ src/test/java/com/example/innovateiu_home_task/InnovateIuHomeTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.innovateiu_home_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovateIuHomeTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
